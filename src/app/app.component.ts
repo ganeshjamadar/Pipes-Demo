6537ee33950f214bd351b20e5a21ce6066f2cda0
Ganesh.Jamadar <Ganesh.Jamadar@IL013511>
2020-08-05 12:54:13 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  
+import { resolve } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes-Demo';
+  appStatus = new Promise(
+    (resolve, reject) => {
+      setTimeout(() => {
+        resolve('stable');
+      }, 2000);
+    }
+  );
+  filterStatus = '';
+  propName = '';
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'offline',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    },{
+      instanceType: 'small',
+      name: 'Test Enviroment Server',
+      status: 'critical',
+      started: new Date(15,1,2017)
+    },
+  ];
+
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}){
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical',
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ShortenPipe, FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
